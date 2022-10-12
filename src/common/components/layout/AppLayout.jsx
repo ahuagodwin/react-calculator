9c7ab04157448d40a9ca28e89233216050f658a3
Ahua Godwin <57255108+ahuagodwin@users.noreply.github.com>
2022-10-12 12:22:15 +0100

    refactoring

--- src/common/components/layout/AppLayout.jsx
+++ src/common/components/layout/AppLayout.jsx
@@ -6,8 +6,8 @@ const AppLayout = ({ children }) => {
   return (
     <div className='flex h-[100vh] justify-center items-center theme-bg'>
         {children}
-        <div className='absolute bottom-4 right-4'>
-          <h2 className="text-slate-900">Godwin Ahua | Front-end Developer</h2>
+        <div className='absolute right-4 bottom-4'>
+          <h2 className="text-slate-900">Godwin Ahua | Front-end Developer (REACT JS)</h2>
         </div>
     </div>
   )
